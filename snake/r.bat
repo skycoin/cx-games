5bac68e177507a310299c0656a03ac5370517e1e
corpusc <corpus_callosum@hotmail.com>
2017-12-24 13:35:12 -0500

    + entity.cx,   & refactors

--- snake/r.bat
+++ snake/r.bat
@@ -8,6 +8,6 @@ IF "%1" == "c" go install %gopath%/github.com/skycoin/cx/cx/
 IF "%1" == "c" dir %gopath%\bin\cx*
 IF "%1" == "c" GOTO END
 
-%gopath%\bin\cx game.cx main.cx
+%gopath%\bin\cx game.cx main.cx entity.cx
 
 :END
