eb5f6377157bd8e54c9cc9d340e5d4565146a30f
corpusc <corpus_callosum@hotmail.com>
2017-12-24 02:15:37 -0500

    lightly refactored copy of bouncing-ball-control.cx

---
+++ snake/r.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+ECHO *****************************************************************
+ECHO               Pass "c" to compile latest CX
+ECHO *****************************************************************
+
+IF "%1" == "c" go install %gopath%/github.com/skycoin/cx/cx/
+IF "%1" == "c" dir %gopath%\bin\cx*
+IF "%1" == "c" GOTO END
+
+%gopath%\bin\cx main.cx
+
+:END
