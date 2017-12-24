86561309f8497cc7bc34ffe57ba7aa62188ebc5d
corpusc <corpus_callosum@hotmail.com>
2017-12-24 17:02:33 -0500

    + initial random "map"

--- snake/r.bat
+++ snake/r.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 ECHO *****************************************************************
-ECHO               Pass "c" to compile latest CX
+ECHO                Pass "c" to compile latest CX
 ECHO *****************************************************************
 
 IF "%1" == "c" go install %gopath%/github.com/skycoin/cx/cx/
