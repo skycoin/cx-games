7c86bf23d330983a51773a7d47cfd89293854aa1
corpusc <corpus_callosum@hotmail.com>
2017-12-31 19:02:08 -0500

    + 2 buggy folders,   better init positions, ...
    ...   + snake head,   + GLFW key code info for manual reference

--- snake/r.bat
+++ snake/r.bat
@@ -4,7 +4,7 @@ ECHO *****************************************************************
 ECHO                Pass "c" to compile latest CX
 ECHO *****************************************************************
 
-IF "%1" == "c" go install %gopath%/github.com/skycoin/cx/cx/
+IF "%1" == "c" go install github.com/skycoin/cx/cx/
 IF "%1" == "c" dir %gopath%\bin\cx*
 IF "%1" == "c" GOTO END
 
