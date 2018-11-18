e89691f73ba1afe55f366ba1c28908b933f00fcd
corpusc <corpus_callosum@hotmail.com>
2018-11-18 18:13:05 -0500

    + collision.cx,   & moved ball updating to entitycx,  & hooked up draw.cx + moved circle drawing into it

--- ridge-blaster/c.bat
+++ ridge-blaster/c.bat
@@ -1,3 +1,2 @@
 dir
-rem cx app.cx entity.cx environs.cx input.cx draw.cx game.cx main.cx
-cx main.cx app.cx game.cx input.cx entity.cx environs.cx
+cx main.cx app.cx game.cx input.cx entity.cx environs.cx draw.cx collision.cx
