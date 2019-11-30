8a4e6a3d12f87b0df5bb24550eae524a7ba25ecd
corpusc <corpus_callosum@hotmail.com>
2019-11-30 17:03:25 -0500

    Sky-Taxi: ....

--- Sky-Taxi/run.bat
+++ Sky-Taxi/run.bat
@@ -1,1 +1,4 @@
+echo DOING THIS ********************* cx ..\2DFWork\. .
+echo DOING THIS ********************* cx ..\2DFWork\. .
+echo DOING THIS ********************* cx ..\2DFWork\. .
 cx ..\2DFWork\. .
