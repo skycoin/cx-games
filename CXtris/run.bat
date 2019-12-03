6ab6ea708b65e9a86152b654a855c08547993c44
corpusc <corpus_callosum@hotmail.com>
2019-12-03 00:08:29 -0500

    ....

--- CXtris/run.bat
+++ CXtris/run.bat
@@ -1,13 +1,13 @@
-rem @echo off
-rem **************************************************************************************
-rem **************************************************************************************
-rem **************************************************************************************
-rem ***** ONCE REFACTORED TO USE SHARED PACKAGES IN 2DFWork, WE'LL USE THE NEXT LINE *****
-rem **************************************************************************************
-rem **************************************************************************************
-rem **************************************************************************************
+@echo off
+
+echo tested with CX version 0.7.0 *********************
+echo tested with CX version 0.7.0 *********************
+echo tested with CX version 0.7.0 *********************
+echo tested with CX version 0.7.0 *********************
+
+echo ********** running with command  cx ..\2DFWork\. .
+echo ********** running with command  cx ..\2DFWork\. .
+echo ********** running with command  cx ..\2DFWork\. .
+echo ********** running with command  cx ..\2DFWork\. .
 
 cx ..\2DFWork\. .
-
-
-rem cx shape\shape.cx shape\shape-init.cx shape\shape-next.cx draw\draw.cx draw\draw-text.cx ..\2DFWork\inputs\inputs-keys.cx inputs\inputs.cx app.cx main.cx map.cx menu.cx space.cx time.cx
